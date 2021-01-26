45c889966ea223f981a80c70c2af6ac6942125a1
Mark Fawaz <markfawa@amazon.com>
2021-01-26 09:48:36 -0800

    Update config file parsing to use json objects to avoid parsing issues

--- src/CTA.Rules.Actions/ActionHelpers/ConfigMigrate.cs
+++ src/CTA.Rules.Actions/ActionHelpers/ConfigMigrate.cs
@@ -7,6 +7,8 @@ using System.Text;
 using System.Xml.Linq;
 using System.Linq;
 using System.Xml.Serialization;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
 
 namespace CTA.Rules.Actions
 {
@@ -14,13 +16,13 @@ namespace CTA.Rules.Actions
     {
         private string _projectDir;
         private ProjectType _projectType;
-
-
+        private bool _hasData;
 
         public ConfigMigrate(string projectDir, ProjectType projectType)
         {
             _projectDir = Directory.GetParent(projectDir).FullName;
             _projectType = projectType;
+            _hasData = false;
         }
         public string Run()
         {
@@ -38,11 +40,11 @@ namespace CTA.Rules.Actions
             var configXml = LoadWebConfig(_projectDir);
             if (configXml == null) { return isConfigFound; }
 
-            var config = ProcessWebConfig(configXml);
-            if (!string.IsNullOrEmpty(config))
+            var config = ProcessWebConfig(configXml, TemplateHelper.GetTemplateFileContent("", _projectType, Constants.appSettingsJson));
+            if (_hasData)
             {
                 isConfigFound = "Found and migrated settings from web.config";
-                AddAppSettingsJsonFile(config, TemplateHelper.GetTemplateFileContent("", _projectType, Constants.appSettingsJson), _projectDir);
+                AddAppSettingsJsonFile(config, _projectDir);
             }
 
             return isConfigFound;
@@ -67,40 +69,71 @@ namespace CTA.Rules.Actions
         }
 
         /// <summary>
-        /// Deserialize web.config file to an object
+        /// Reads data from web.config and writes any relevant entries to appsettings.json file
         /// </summary>
-        /// <param name="projectDir">The project directory containing the web.config file</param>
-        /// <returns></returns>
-        private string ProcessWebConfig(XDocument webConfig)
+        /// <param name="webConfig">The XML representation of the web.config file</param>
+        /// <returns>A JSON object representing the appSettings.json file </returns>
+        private JObject ProcessWebConfig(XDocument webConfig, string templateContent)
         {
-            StringBuilder config = new StringBuilder();
+            var connectionStringsObjects = GetConnectionStrings(webConfig);
+            var appSettingsObjects = GetAppSettingObjects(webConfig);
 
+            if(_hasData)
+            {
+                var defaultContent = JsonConvert.DeserializeObject<JObject>(templateContent);
+
+                if (connectionStringsObjects.Count > 0)
+                {
+                    AddToJsonObject(defaultContent, Constants.ConnectionStrings, connectionStringsObjects);
+                }
+                if (appSettingsObjects.Count > 0)
+                {
+                    AddToJsonObject(defaultContent, Constants.appSettings, appSettingsObjects);
+                }
+                return defaultContent;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets connection strings from web.config file
+        /// </summary>
+        /// <param name="webConfig"></param>
+        /// <returns></returns>
+        private Dictionary<string, string> GetConnectionStrings(XDocument webConfig)
+        {
+            Dictionary<string, string> connectionStringObjects = new Dictionary<string, string>();
             webConfig.Descendants()?.Where(d => d.Name.ToString().ToLower() == Constants.connectionstrings)?.Descendants()?.ToList().ForEach(connectionString =>
             {
                 try
                 {
                     var name = connectionString.Attributes()?.First(a => a.Name?.ToString().ToLower() == Constants.name).Value;
-                    var value = connectionString.Attributes()?.First(a => a.Name?.ToString().ToLower() == Constants.connectionstring).Value?.Replace(@"\", @"\\");
+                    var value = connectionString.Attributes()?.First(a => a.Name?.ToString().ToLower() == Constants.connectionstring).Value;
 
-                    config.Append("\"").Append(name).Append("\"").Append(":").Append("\"").Append(value).Append("\"").Append(",");
+                    connectionStringObjects.Add(name, value);
+                    _hasData = true;
                 }
                 catch (Exception ex)
                 {
                     LogHelper.LogError(ex, "Error while parsing connection string");
                 }
             });
+            return connectionStringObjects;
+        }
 
-            //This means, we found at least one connection string:
-            if (config.Length > 0)
-            {
-                config.Remove(config.Length - 1, 1);
-                config.Insert(0, "\": { ");
-                config.Insert(0, Constants.ConnectionStrings);
-                config.Insert(0, "\"");
-                config.Append("},");
-            }
+        /// <summary>
+        /// Gets app settings from web.config file
+        /// </summary>
+        /// <param name="webConfig"></param>
+        /// <returns></returns>
+        private Dictionary<string, string> GetAppSettingObjects(XDocument webConfig)
+        {
+            Dictionary<string, string> appSettingsObjects = new Dictionary<string, string>();
 
-            webConfig.Descendants()?.First(d => d.Name?.ToString().ToLower() == Constants.appSettings)?.Descendants()?.ToList().ForEach(appSetting =>
+            webConfig.Descendants()?.Where(d => d.Name?.ToString().ToLower() == Constants.appSettings)?.Descendants()?.ToList().ForEach(appSetting =>
             {
                 try
                 {
@@ -108,7 +141,8 @@ namespace CTA.Rules.Actions
                     var value = appSetting.Attributes()?.First(a => a.Name?.ToString().ToLower() == Constants.value).Value;
                     if (!Constants.appSettingsExclusions.Contains(key))
                     {
-                        config.Append("\"").Append(key).Append("\"").Append(":").Append("\"").Append(value).Append("\"").Append(",");
+                        appSettingsObjects.Add(key, value);
+                        _hasData = true;
                     }
                 }
                 catch (Exception ex)
@@ -117,26 +151,33 @@ namespace CTA.Rules.Actions
                 }
             });
 
-            return config.ToString();
+            return appSettingsObjects;
         }
 
-
+        /// <summary>
+        /// Adds data to json object
+        /// </summary>
+        /// <param name="jObject">The object to add data to</param>
+        /// <param name="name">The name of the new entry</param>
+        /// <param name="arrayContent">The content of the new entry</param>
+        private void AddToJsonObject(JObject jObject, string name, Dictionary<string, string> arrayContent)
+        {
+            jObject.AddFirst(
+                new JProperty(name,
+                new JArray(
+                    arrayContent.Select(c =>
+                    new JObject(new JProperty(c.Key, c.Value))))
+                ));
+        }
 
         /// <summary>
-        /// Adds a config (json) file using the project template, then populates it with data from appsettings and connectionstrings in web.config
-        /// </summary>
-        /// <param name="webConfig">The object representing the web.config file</param>
-        /// <param name="templateContent">The template file to be used</param>
+        /// Writes the appSettings.json file to the project dir
+        /// </summary>        
+        /// <param name="content">The content of the file</param>
         /// <param name="projectDir">The project directory where this file will be created</param>
-        private void AddAppSettingsJsonFile(string content, string templateContent, string projectDir)
+        private void AddAppSettingsJsonFile(JObject content, string projectDir)
         {
-            string fileContent = templateContent.Insert(1, content);
-
-            var obj = JsonConvert.DeserializeObject(fileContent);
-            fileContent = JsonConvert.SerializeObject(obj, Formatting.Indented);
-
-            // Any escaped backslashes were duplicated when the content was deserialized and serialized
-            File.WriteAllText(Path.Combine(projectDir, Constants.appSettingsJson), fileContent.Replace(@"\\", @"\"));
+            File.WriteAllText(Path.Combine(projectDir, Constants.appSettingsJson), content.ToString());
             LogChange(string.Format("Create appsettings.json file using web.config settings"));
         }
         private void LogChange(string message)
--- tst/CTA.Rules.Test/Actions/ActionHelpers/ConfigMigrateTests.cs
+++ tst/CTA.Rules.Test/Actions/ActionHelpers/ConfigMigrateTests.cs
@@ -4,6 +4,7 @@ using System.Reflection;
 using System.Xml.Linq;
 using CTA.Rules.Actions;
 using CTA.Rules.Models;
+using Newtonsoft.Json.Linq;
 using NUnit.Framework;
 
 namespace CTA.Rules.Test.Actions.ActionHelpers
@@ -23,7 +24,7 @@ namespace CTA.Rules.Test.Actions.ActionHelpers
 
             var appSetting1 = @"ClientValidationEnabled";
             var appSetting2 = @"UnobtrusiveJavaScriptEnabled";
-            var connectionString1 = @"Data Source=(LocalDb)\v11.0;Initial Catalog=ContosoUniversity2;Integrated Security=SSPI;";
+            var connectionStringWithBackSlash = @"Data Source=(LocalDb)\v11.0;Initial Catalog=ContosoUniversity2;Integrated Security=SSPI;";
             var connectionString2 = @"Data Source=Test;Initial Catalog=ContosoUniversity1;Integrated Security=SSPI;";
 
             var webConfig = XDocument.Parse(string.Format(@"
@@ -31,7 +32,7 @@ namespace CTA.Rules.Test.Actions.ActionHelpers
     <appSettings>
         <add key=""webpages:Version"" value=""1.0.0.0""/>
         <add key=""{0}"" value=""1.0.0.0""/>
-        <add key=""{1}"" value=""1.0.0.0""/>
+        <add key=""{1}"" value=""VALUEWITH\Backslash""/>
     </appSettings>
     <runtime>
         <assemblyBinding xmlns=""urn:schemas-microsoft-com:asm.v1"">
@@ -46,16 +47,16 @@ namespace CTA.Rules.Test.Actions.ActionHelpers
         <add name=""MvcMusicStoreAuth"" connectionString=""{3}"" providerName=""System.Data.SqlClient"" />
     </connectionStrings> 
 </configuration>
-", appSetting1, appSetting2, connectionString1, connectionString2));
+", appSetting1, appSetting2, connectionStringWithBackSlash, connectionString2));
 
-            var template = @"{}";
+            var templateContent = @"{}";
             var outputDir = "";
 
 
             // Invoke method and read contents of method output
-            var content = (string)processWebConfigMethod.Invoke(configMigrateInstance, new object[] { webConfig });
+            var content = (JObject)processWebConfigMethod.Invoke(configMigrateInstance, new object[] { webConfig, templateContent });
             
-            var methodParams = new object[] { content, template, outputDir };
+            var methodParams = new object[] { content, outputDir };
             addAppSettingsJsonFileMethod.Invoke(configMigrateInstance, methodParams);
 
             var appSettingsContent = File.ReadAllText(Path.Combine(outputDir, "appsettings.json"));
@@ -63,10 +64,57 @@ namespace CTA.Rules.Test.Actions.ActionHelpers
 
             Assert.True(appSettingsContent.Contains(appSetting1));
             Assert.True(appSettingsContent.Contains(appSetting2));
-            Assert.True(appSettingsContent.Contains(connectionString1));
+            Assert.True(appSettingsContent.Contains(connectionStringWithBackSlash.Replace(@"\", @"\\")));
             Assert.True(appSettingsContent.Contains(connectionString2));
         }
 
+        [Test]
+        public void AddConfigWithoutAppSettings()
+        {
+            // Get private method to invoke
+            var projectType = ProjectType.ClassLibrary;
+            ConfigMigrate configMigrateInstance = new ConfigMigrate(Directory.GetCurrentDirectory(), projectType);
+            Type configMigrateType = configMigrateInstance.GetType();
+
+            var processWebConfigMethod = TestUtils.GetPrivateMethod(configMigrateType, "ProcessWebConfig");
+            var addAppSettingsJsonFileMethod = TestUtils.GetPrivateMethod(configMigrateType, "AddAppSettingsJsonFile");
+
+            var connectionStringWithBackSlash = @"Data Source=(LocalDb)\v11.0;Initial Catalog=ContosoUniversity2;Integrated Security=SSPI;";
+            var connectionString2 = @"Data Source=Test;Initial Catalog=ContosoUniversity1;Integrated Security=SSPI;";
+
+            var webConfig = XDocument.Parse(string.Format(@"
+<configuration>
+    <runtime>
+        <assemblyBinding xmlns=""urn:schemas-microsoft-com:asm.v1"">
+            <dependentAssembly>
+                <assemblyIdentity name=""System.Web.Mvc"" publicKeyToken=""31bf3856ad364e35""/>
+                <bindingRedirect oldVersion=""3.0.0.0-3.0.0.1"" newVersion=""3.0.0.1""/>
+            </dependentAssembly>
+        </assemblyBinding>
+    </runtime>
+    <connectionStrings>
+        <add name=""MusicStoreEntities"" connectionString=""{0}"" providerName=""System.Data.SqlClient""/>
+        <add name=""MvcMusicStoreAuth"" connectionString=""{1}"" providerName=""System.Data.SqlClient"" />
+    </connectionStrings> 
+</configuration>
+", connectionStringWithBackSlash, connectionString2));
+
+            var templateContent = @"{}";
+            var outputDir = "";
+
+
+            // Invoke method and read contents of method output
+            var content = (JObject)processWebConfigMethod.Invoke(configMigrateInstance, new object[] { webConfig, templateContent });
+
+            var methodParams = new object[] { content, outputDir };
+            addAppSettingsJsonFileMethod.Invoke(configMigrateInstance, methodParams);
+
+            var appSettingsContent = File.ReadAllText(Path.Combine(outputDir, "appsettings.json"));
+            File.Delete(Path.Combine(outputDir, "appsettings.json"));
+
+            Assert.True(appSettingsContent.Contains(connectionStringWithBackSlash.Replace(@"\", @"\\")));
+            Assert.True(appSettingsContent.Contains(connectionString2));
+        }
 
         [Test]
         public void LoadWebConfigWithErrors()
