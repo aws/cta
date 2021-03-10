42d4ad6f1d1d20eda1cc6bf6a70e73bccdfc32b4
Jon Louie <louiejon@amazon.com>
2021-03-10 08:09:31 -0800

    Add WindowsAuthentication feature

---
+++ src/CTA.FeatureDetection.AuthType/CompiledFeatures/WindowsAuthenticationFeature.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using Codelyzer.Analysis;
+using CTA.FeatureDetection.Common.Extensions;
+
+namespace CTA.FeatureDetection.AuthType.CompiledFeatures
+{
+    public class WindowsAuthenticationFeature : WebConfigFeature
+    {
+        private readonly string _path = $"{Constants.ConfigurationElement}/{Constants.SystemWebElement}/{Constants.AuthenticationElement}";
+
+        public override bool IsPresent(AnalyzerResult analyzerResult)
+        {
+            var configs = LoadWebConfigs(analyzerResult);
+            return configs.Any(c => c.ContainsAttributeValue(_path, Constants.ModeAttribute, Constants.WindowsAuthenticationType));
+        }
+    }
+}
