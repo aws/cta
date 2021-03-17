fb66b7fc7fefb371a59dc6e694be2453991d8ab4
Jon Louie <louiejon@amazon.com>
2021-03-17 16:28:34 -0700

    add api criteria to FederatedAuth

--- src/CTA.FeatureDetection.AuthType/CompiledFeatures/FederatedAuthenticationFeature.cs
+++ src/CTA.FeatureDetection.AuthType/CompiledFeatures/FederatedAuthenticationFeature.cs
@@ -1,4 +1,6 @@
-﻿using Codelyzer.Analysis;
+﻿using System.Linq;
+using Codelyzer.Analysis;
+using Codelyzer.Analysis.Model;
 using CTA.Rules.Common.WebConfigManagement;
 
 namespace CTA.FeatureDetection.AuthType.CompiledFeatures
@@ -17,14 +19,27 @@ namespace CTA.FeatureDetection.AuthType.CompiledFeatures
         ///        </authentication>
         ///      </system.web>
         ///    </configuration>
+        ///
+        /// 2. IAppBuilder.UseWsFederationAuthentication(...)
         /// 
         /// </summary>
         /// <param name="analyzerResult">Source code analysis results</param>
         /// <returns>Whether or not Federated Authentication is used</returns>
         public override bool IsPresent(AnalyzerResult analyzerResult)
         {
+            return IsPresentInConfig(analyzerResult) || IsPresentInCode(analyzerResult);
+        }
+
+        public bool IsPresentInConfig(AnalyzerResult analyzerResult)
+        {
             var config = WebConfigManager.LoadWebConfigAsXDocument(analyzerResult.ProjectResult.ProjectRootPath);
             return config.ContainsAttributeWithValue(Constants.AuthenticationElementElementPath, Constants.ModeAttribute, Constants.FederatedAuthenticationType);
+        }
+
+        public bool IsPresentInCode(AnalyzerResult analyzerResult)
+        {
+            return analyzerResult.ProjectResult.SourceFileResults.Any(s => 
+                s.AllInvocationExpressions().Any(i => i.SemanticOriginalDefinition.StartsWith(Constants.WsFederationAuthenticationQualifiedName)));
         }
     }
 }
