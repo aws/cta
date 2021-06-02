9d2e5c277f65bfb177fa24af61aed13aeee77de3
Jon Louie <louiejon@amazon.com>
2021-06-02 14:18:08 -0700

    optimize feature detection tests

--- tst/CTA.FeatureDetection.Tests/CTA.FeatureDetection.EntityFramework/EntityFrameworkFeatureDetectionTests.cs
+++ tst/CTA.FeatureDetection.Tests/CTA.FeatureDetection.EntityFramework/EntityFrameworkFeatureDetectionTests.cs
@@ -1,26 +1,17 @@
 ﻿using CTA.FeatureDetection.Common.Models;
-using CTA.FeatureDetection.Tests.Utils;
 using NUnit.Framework;
-using System.IO;
-using System.Linq;
 
 namespace CTA.FeatureDetection.Tests.FeatureDetection.EntityFramework
 {
     public class EntityFrameworkFeatureDetectionTests
     {
         private const string ProjectName = "EF6_Test.csproj";
-        private static readonly string TestProjectDirectory = TestUtils.GetTestAssemblySourceDirectory(typeof(TestUtils));
-        private static string ConfigFile => Path.Combine(TestProjectDirectory, "Examples", "Templates", "EntityFramework_config_features.json");
         private FeatureDetectionResult _featureDetectionResult;
 
         [SetUp]
         public void SetUp()
         {
-            var analyzerResult = TestProjectsSetupFixture.EfAnalyzerResults.First(a =>
-                a.ProjectResult.ProjectFilePath == TestProjectsSetupFixture.Ef6ProjectPath);
-
-            var featureDetector = new FeatureDetector(ConfigFile);
-            _featureDetectionResult = featureDetector.DetectFeaturesInProject(analyzerResult);
+            _featureDetectionResult = TestProjectsSetupFixture.Ef6FeatureDetectionResult;
         }
 
         [Test]
