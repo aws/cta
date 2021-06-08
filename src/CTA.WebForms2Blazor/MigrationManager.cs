90d534da712f9c1b49f12d90a90146f83029d96c
Emery Comstock <comsemer@amazon.com>
2021-06-07 17:01:51 -0700

    Added WebForms2Blazor Project and Test Project
    Added basically empty WebForms2Blazor project and accompanying test
    project using NUnit.

---
+++ src/CTA.WebForms2Blazor/MigrationManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CTA.WebForms2Blazor
+{
+    public class MigrationManager
+    {
+        private readonly string _sourceProjectPath;
+        private readonly string _outputProjectPath;
+
+        public MigrationManager(string sourceProjectPath, string outputProjectPath)
+        {
+            _sourceProjectPath = sourceProjectPath;
+            _outputProjectPath = outputProjectPath;
+
+            // Initialize any objects or services needed for migration here
+        }
+
+        public void PerformMigration()
+        {
+            // Central migration logic goes here
+        }
+    }
+}
---
+++ tst/CTA.WebForms2Blazor.Tests/MigrationManagerTests.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace CTA.WebForms2Blazor.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            // Do setup
+        }
+
+        [Test]
+        public void ExampleTest()
+        {
+            Assert.Pass();
+        }
+    }
+}
