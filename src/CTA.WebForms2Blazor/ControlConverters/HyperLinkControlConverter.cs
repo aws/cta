85360cde28d625a2964af08c4a263271fcc34da4
KallanBao <baokalla@88665a2a03e5.ant.amazon.com>
2021-07-01 14:52:39 -0700

    addressed 2nd round PR change request

--- src/CTA.WebForms2Blazor/ControlConverters/HyperLinkControlConverter.cs
+++ src/CTA.WebForms2Blazor/ControlConverters/HyperLinkControlConverter.cs
@@ -19,31 +19,5 @@ namespace CTA.WebForms2Blazor.ControlConverters
         }
         protected override string BlazorName { get { return "a"; } }
 
-        public HyperLinkControlConverter() : base()
-        {
-            
-        }
-
-        public override HtmlNode Convert2Blazor(HtmlNode node)
-        {
-            string newAttributes = "";   
-            foreach (HtmlAttribute attr in node.Attributes)
-            {
-                if (AttributeMap.ContainsKey(attr.Name))
-                {
-                    string newAttr = AttributeMap[attr.Name] + "=" + attr.Value + " ";
-                    newAttributes += newAttr;
-                }
-            }
-
-            newAttributes = newAttributes.Trim();
-
-            string template = 
-                @"<{0} {1}>{2}</{0}>";
-            
-            string newContent = String.Format(template, BlazorName, newAttributes, node.InnerHtml);
-            HtmlNode newNode = HtmlNode.CreateNode(newContent);
-            return newNode;
-        }
     }
 }
