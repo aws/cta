50f09ee5a03af9c89910e8402f6b217adf486d66
Jon Louie <louiejon@amazon.com>
2021-12-08 13:19:48 -0800

    create project rewriter factories

---
+++ src/CTA.Rules.PortCore/ProjectSpecificPort/ProjectRewriters/PortCoreProjectRewriterFactory.cs
@@ -0,0 +1,32 @@
+﻿using Codelyzer.Analysis;
+using Codelyzer.Analysis.Build;
+using CTA.Rules.Models;
+using CTA.Rules.Update;
+
+namespace CTA.Rules.PortCore
+{
+    public class PortCoreProjectRewriterFactory : IProjectRewriterFactory
+    {
+        public ProjectRewriter GetInstance(AnalyzerResult analyzerResult, ProjectConfiguration projectConfiguration)
+        {
+            var projectType = projectConfiguration.ProjectType;
+            var projectRewriter = projectType switch
+            {
+                ProjectType.WebForms => new WebFormsProjectRewriter( analyzerResult, projectConfiguration),
+                _ => new ProjectRewriter(analyzerResult, projectConfiguration)
+            };
+            return projectRewriter;
+        }
+
+        public ProjectRewriter GetInstance(IDEProjectResult ideProjectResult, ProjectConfiguration projectConfiguration)
+        {
+            var projectType = projectConfiguration.ProjectType;
+            var projectRewriter = projectType switch
+            {
+                ProjectType.WebForms => new WebFormsProjectRewriter(ideProjectResult, projectConfiguration),
+                _ => new ProjectRewriter(ideProjectResult, projectConfiguration)
+            };
+            return projectRewriter;
+        }
+    }
+}
---
+++ src/CTA.Rules.Update/ProjecRewriters/IProjectRewriterFactory.cs
@@ -0,0 +1,12 @@
+﻿using Codelyzer.Analysis;
+using Codelyzer.Analysis.Build;
+using CTA.Rules.Models;
+
+namespace CTA.Rules.Update
+{
+    public interface IProjectRewriterFactory
+    {
+        public ProjectRewriter GetInstance(AnalyzerResult analyzerResult, ProjectConfiguration projectConfiguration);
+        public ProjectRewriter GetInstance(IDEProjectResult ideProjectResult, ProjectConfiguration projectConfiguration);
+    }
+}
