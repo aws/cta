8a9881951b39f8d0e061ddd8d8f4a1f7ff63bc92
Emery Comstock <comsemer@amazon.com>
2021-12-14 09:51:52 -0800

    Improved namespace normalization
    - Hyphens and spaces converted to underscores
    - Modified some existing tests and added new ones

--- tst/CTA.WebForms2Blazor.Tests/Helpers/UtilitiesTests.cs
+++ tst/CTA.WebForms2Blazor.Tests/Helpers/UtilitiesTests.cs
@@ -18,7 +18,7 @@ namespace CTA.WebForms2Blazor.Tests.Helpers
         [Test]
         public void NormalizeNamespaceIdentifier_Removes_Invalid_Characters()
         {
-            var input = "Test$Na!me^sp~ace@2@.##Sub-Namespace##";
+            var input = "Test$Na!me^sp~ace@2@.##Sub*Namespace##";
             var expectedOutput = "TestNamespace2.SubNamespace";
             var output = Utilities.NormalizeNamespaceIdentifier(input);
 
@@ -40,6 +40,36 @@ namespace CTA.WebForms2Blazor.Tests.Helpers
         {
             var input = "TestNamespace4.$.SubNamespace...@..###..%.SubSubNamespace";
             var expectedOutput = "TestNamespace4.SubNamespace.SubSubNamespace";
+            var output = Utilities.NormalizeNamespaceIdentifier(input);
+
+            Assert.AreEqual(expectedOutput, output);
+        }
+
+        [Test]
+        public void NormalizeNamespaceIdentifier_Replaces_Hyphens_With_Underscores()
+        {
+            var input = "TestNamespace-5.Sub--Namespace.Sub--Sub--Namespace";
+            var expectedOutput = "TestNamespace_5.Sub__Namespace.Sub__Sub__Namespace";
+            var output = Utilities.NormalizeNamespaceIdentifier(input);
+
+            Assert.AreEqual(expectedOutput, output);
+        }
+
+        [Test]
+        public void NormalizeNamespaceIdentifier_Replaces_Spaces_With_Underscores()
+        {
+            var input = "TestNamespace 6.Sub  Namespace.Sub  Sub  Namespace";
+            var expectedOutput = "TestNamespace_6.Sub__Namespace.Sub__Sub__Namespace";
+            var output = Utilities.NormalizeNamespaceIdentifier(input);
+
+            Assert.AreEqual(expectedOutput, output);
+        }
+
+        [Test]
+        public void NormalizeNamespaceIdentifier_Replaces_Mixed_Hyphens_And_Spaces_With_Underscores()
+        {
+            var input = "TestNamespace-7.Sub  Namespace.Sub- Sub -Namespace";
+            var expectedOutput = "TestNamespace_7.Sub__Namespace.Sub__Sub__Namespace";
             var output = Utilities.NormalizeNamespaceIdentifier(input);
 
             Assert.AreEqual(expectedOutput, output);
