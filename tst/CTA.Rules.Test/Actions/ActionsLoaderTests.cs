02fe5ecd7d0d8f8b9fd6649abc737e0caecd47de
Jonathan Boudreault <boudreaj@amazon.com>
2022-01-28 12:22:01 -0800

    fix renaming of action test class

--- tst/CTA.Rules.Test/Actions/ActionsLoaderTests.cs
+++ tst/CTA.Rules.Test/Actions/ActionsLoaderTests.cs
@@ -10,7 +10,7 @@ using System.Collections.Generic;
 
 namespace CTA.Rules.Test.Actions
 {
-    public class AttributeAActionsLoaderTests
+    public class ActionLoaderTests
     {
         ActionsLoader _actionLoader;
 
