9a1016c48adf1641930a2e3c76bcbc0d75adfa94
Zhang <mingxzh@amazon.com>
2022-02-17 13:47:27 -0800

    fix: rename NoComments to RemoveAllTrivia based on the calling context. Added unit test for the extension code as well.

--- src/CTA.FeatureDetection.Common/Extensions/SyntaxNodeExtensions.cs
+++ src/CTA.FeatureDetection.Common/Extensions/SyntaxNodeExtensions.cs
@@ -7,18 +7,18 @@ namespace CTA.FeatureDetection.Common.Extensions
 {
     public static class SyntaxNodeExtensions
     {
-        // A syntax tree has SyntaxNodes, SyntaxTokens and SyntaxTrivia, for more detail please refer to link below.
+        // A syntax tree has SyntaxNodes, SyntaxTokens and SyntaxTrivia, for more detail please refer to link below:
         // https://docs.microsoft.com/en-us/dotnet/csharp/roslyn-sdk/get-started/syntax-analysis
-        // This function is to remove all comments from the syntax tree, which it needs to iterate and replaces all
-        // SyntaxNodes and SyntaxTokens to so no comments or white spaces are in the tree anymore.
-        public static SyntaxNode NoComments(this SyntaxNode node)
+        // This function's purpose is to remove all trivia from the syntax tree, so it needs to iterate through all
+        // existing SyntaxNodes and SyntaxTokens and to replace each of them without trivia.
+        public static SyntaxNode RemoveAllTrivia(this SyntaxNode node)
         {
             if (node != null)
             {
                 node = node.WithoutTrivia();
 
                 var childNodes = node.ChildNodes();
-                node = node.ReplaceNodes(childNodes, (oldNode, _) => oldNode.NoComments());
+                node = node.ReplaceNodes(childNodes, (oldNode, _) => oldNode.RemoveAllTrivia());
 
                 var childTokens = node.ChildTokens();
                 node = node.ReplaceTokens(childTokens, (oldToken, _) => oldToken.WithoutTrivia());
