ce9bf905512cec994b24d041c4d8c8a7f50633aa
Zhang <mingxzh@amazon.com>
2022-02-16 22:04:40 -0800

    fix: SyntaxNodeExtension NoComments function is not removing all comments from Syntax Tree.

--- src/CTA.FeatureDetection.Common/Extensions/SyntaxNodeExtensions.cs
+++ src/CTA.FeatureDetection.Common/Extensions/SyntaxNodeExtensions.cs
@@ -7,13 +7,21 @@ namespace CTA.FeatureDetection.Common.Extensions
 {
     public static class SyntaxNodeExtensions
     {
+        // A syntax tree has SyntaxNodes, SyntaxTokens and SyntaxTrivia, for more detail please refer to link below.
+        // https://docs.microsoft.com/en-us/dotnet/csharp/roslyn-sdk/get-started/syntax-analysis
+        // This function is to remove all comments from the syntax tree, which it needs to iterate and replaces all
+        // SyntaxNodes and SyntaxTokens to so no comments or white spaces are in the tree anymore.
         public static SyntaxNode NoComments(this SyntaxNode node)
         {
             if (node != null)
             {
-                var childNodes = node.ChildNodes();
-                node = node.ReplaceNodes(childNodes, (node, returnedNode) => node.NoComments());
                 node = node.WithoutTrivia();
+
+                var childNodes = node.ChildNodes();
+                node = node.ReplaceNodes(childNodes, (oldNode, _) => oldNode.NoComments());
+
+                var childTokens = node.ChildTokens();
+                node = node.ReplaceTokens(childTokens, (oldToken, _) => oldToken.WithoutTrivia());
             }
             return node;
         }
