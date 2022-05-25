c5e4b82ebd8a483306aba7643ed49e8c9bd55125
longachr <longachr@amazon.com>
2022-05-25 15:19:52 -0700

    Add element access actions for vb

---
+++ src/CTA.Rules.Actions/VisualBasic/ElementAccessActions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using CTA.Rules.Config;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.VisualBasic;
+using Microsoft.CodeAnalysis.VisualBasic.Syntax;
+using Microsoft.CodeAnalysis.Editing;
+
+namespace CTA.Rules.Update.VisualBasic
+{
+    /// <summary>
+    /// List of actions that can run on Element Accesses
+    /// </summary>
+    public class ElementAccessActions
+    {
+        public Func<SyntaxGenerator, MemberAccessExpressionSyntax, MemberAccessExpressionSyntax> GetAddCommentAction(string comment)
+        {
+            MemberAccessExpressionSyntax AddComment(SyntaxGenerator syntaxGenerator, MemberAccessExpressionSyntax node)
+            {
+                SyntaxTriviaList currentTrivia = node.GetLeadingTrivia();
+                currentTrivia = currentTrivia.Insert(0, SyntaxFactory.SyntaxTrivia(SyntaxKind.CommentTrivia, string.Format(Constants.VbCommentFormat, comment)));
+                node = node.WithLeadingTrivia(currentTrivia).NormalizeWhitespace();
+                return node;
+            }
+            return AddComment;
+        }
+        public Func<SyntaxGenerator, MemberAccessExpressionSyntax, MemberAccessExpressionSyntax> GetReplaceElementAccessAction(string newExpression)
+        {
+            // todo: for VB rules that invoke this action probably need to be converted to invocation expressions because
+            MemberAccessExpressionSyntax ReplaceElement(SyntaxGenerator syntaxGenerator, MemberAccessExpressionSyntax node)
+            {
+                var addCommentFunc = GetAddCommentAction($"Replace with {newExpression}");
+                return addCommentFunc(syntaxGenerator, node);
+            }
+            return ReplaceElement;
+        }
+    }
+}
---
+++ tst/CTA.Rules.Test/Actions/VisualBasic/ElementAccessActionsTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using CTA.Rules.Models;
+using CTA.Rules.Update.VisualBasic;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.VisualBasic;
+using Microsoft.CodeAnalysis.VisualBasic.Syntax;
+using Microsoft.CodeAnalysis.Editing;
+using NUnit.Framework;
+
+namespace CTA.Rules.Test.Actions.VisualBasic
+{
+    public class ElementAccessActionsTests
+    {
+        private ElementAccessActions _elementAccessActions;
+        private SyntaxGenerator _syntaxGenerator;
+        private MemberAccessExpressionSyntax _node;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var workspace = new AdhocWorkspace();
+            var language = LanguageNames.VisualBasic;
+            _syntaxGenerator = SyntaxGenerator.GetGenerator(workspace, language);
+            _elementAccessActions = new ElementAccessActions();
+            _node = SyntaxFactory.MemberAccessExpression(SyntaxKind.SimpleMemberAccessExpression,
+                SyntaxFactory.ParseExpression("Expression"), SyntaxFactory.Token(SyntaxKind.DotToken),
+                (SimpleNameSyntax)SyntaxFactory.ParseName("ElementAccess"));
+        }
+
+        [Test]
+        public void ElementAccessAddComment()
+        {
+            const string comment = "This is a comment";
+            var addCommentFunc = _elementAccessActions.GetAddCommentAction(comment);
+            var newNode = addCommentFunc(_syntaxGenerator, _node);
+            StringAssert.Contains(comment, newNode.ToFullString());
+        }
+
+        [Test]
+        public void ReplaceElementAccess()
+        {
+            const string expression = "ConfigurationManager.Configuration.GetSection(\"ConnectionStrings\")";
+            var replaceElementAccessFunc = _elementAccessActions.GetReplaceElementAccessAction(expression);
+            var newNode = replaceElementAccessFunc(_syntaxGenerator, _node);
+            StringAssert.Contains($"' Added by CTA: Replace with {expression}", newNode.ToFullString());
+        }
+
+        [Test]
+        public void ElementAccessActionComparison()
+        {
+            throw new NotImplementedException();
+            // var elementAccessAction = new ElementAccessAction()
+            // {
+            //     Key = "Test",
+            //     Value = "Test2",
+            //     ElementAccessExpressionActionFunc = _elementAccessActions.GetAddCommentAction("Test")
+            //
+            // };
+            //
+            // var cloned = elementAccessAction.Clone<ElementAccessAction>();
+            //
+            // Assert.True(elementAccessAction.Equals(cloned));
+            // cloned.Value = "DifferentValue";
+            // Assert.False(elementAccessAction.Equals(cloned));
+        }
+    }
+}
