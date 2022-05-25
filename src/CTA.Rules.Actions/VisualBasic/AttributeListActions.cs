ac5430c51ad25a4c527a9ced752f56976542c9de
longachr <longachr@amazon.com>
2022-05-25 13:07:03 -0700

    Add attribute and attribute list actions for vb

---
+++ src/CTA.Rules.Actions/VisualBasic/AttributeListActions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using CTA.Rules.Config;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.VisualBasic;
+using Microsoft.CodeAnalysis.VisualBasic.Syntax;
+using Microsoft.CodeAnalysis.Editing;
+
+namespace CTA.Rules.Actions.VisualBasic
+{
+    /// <summary>
+    /// List of actions that can run on attribute lists
+    /// </summary>
+    public class AttributeListActions
+    {
+        public Func<SyntaxGenerator, AttributeListSyntax, AttributeListSyntax> GetAddCommentAction(string comment)
+        {
+            AttributeListSyntax AddComment(SyntaxGenerator syntaxGenerator, AttributeListSyntax node)
+            {
+                //TODO IS there possibility of NPE , if there are no Trivia or it always returns a node...
+                var currentTrivia = node.GetLeadingTrivia();
+                currentTrivia = currentTrivia.Insert(0,
+                    SyntaxFactory.SyntaxTrivia(SyntaxKind.CommentTrivia,
+                        string.Format(Constants.CommentFormat, comment)));
+                node = node.WithLeadingTrivia(currentTrivia).NormalizeWhitespace();
+                return node;
+            }
+            return AddComment;
+        }
+    }
+}
---
+++ tst/CTA.Rules.Test/Actions/VisualBasic/AttributeActionsTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using CTA.Rules.Actions.VisualBasic;
+using CTA.Rules.Models;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.VisualBasic;
+using Microsoft.CodeAnalysis.VisualBasic.Syntax;
+using Microsoft.CodeAnalysis.Editing;
+using NUnit.Framework;
+
+namespace CTA.Rules.Test.Actions.VisualBasic
+{
+    public class AttributeActionsTests
+    {
+        private const string OriginalAttribute = "Serializable";
+        private AttributeActions _attributeActions;
+        private SyntaxGenerator _syntaxGenerator;
+        private AttributeSyntax _node;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var workspace = new AdhocWorkspace();
+            var language = LanguageNames.VisualBasic;
+            _syntaxGenerator = SyntaxGenerator.GetGenerator(workspace, language);
+            _attributeActions = new AttributeActions();
+            _node = SyntaxFactory.Attribute(SyntaxFactory.IdentifierName(SyntaxFactory.ParseToken(OriginalAttribute)));
+        }
+
+        [Test]
+        public void GetChangeAttributeAction_Changes_Attribute_To_Specified_Value()
+        {
+            const string newAttributeName = "NewAttribute";
+            var changeAttributeFunc = _attributeActions.GetChangeAttributeAction(newAttributeName);
+            var newNode = changeAttributeFunc(_syntaxGenerator, _node);
+
+            Assert.AreEqual(OriginalAttribute, _node.ToString());
+            Assert.AreEqual(newAttributeName, newNode.ToString());
+        }
+
+        [Test]
+        public void AttributeActionComparison()
+        {
+            throw new NotImplementedException();
+            //     
+            // var attributeAction = new AttributeAction()
+            // {
+            //     Key = "Test",
+            //     Value = "Test2",
+            //     AttributeActionFunc = _attributeActions.GetChangeAttributeAction("NewAttribute")
+            // };
+            //
+            // var cloned = attributeAction.Clone<AttributeAction>();
+            //
+            // Assert.True(attributeAction.Equals(cloned));
+            // cloned.Value = "DifferentValue";
+            // Assert.False(attributeAction.Equals(cloned));
+        }
+    }
+}
